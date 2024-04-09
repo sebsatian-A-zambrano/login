29e36816c7676d24898162d49196d5011ecf7d65
sebsatian-A-zambrano <147666479+sebsatian-A-zambrano@users.noreply.github.com>
2024-04-09 08:51:03 -0300

    Add files via upload

---
+++ src/css-js/index.js
@@ -0,0 +1,6 @@
+alert('Digite su nombre')
+alert('No se espera lo que hay ahí detras')
+
+let Nome = prompt("Digitelo aqui Usuario misterioso")
+
+alert('Hola ' + Nome + ' Puede entran a nuestro site, pero mucho cuidadoo!!!!')
